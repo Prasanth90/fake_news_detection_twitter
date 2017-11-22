d69d8724033665979e2f16e881932e67bbda6d4e
prasanth <santhtce@gmail.com>
2017-11-22 02:29:03 -0500

    Added Process data function for 3d SVM

--- src/3Dplot.R
+++ src/3Dplot.R
@@ -1,3 +1,125 @@
+## Data Preprocessing ................
+library(rgl)
+library(stringr)
+options(print.max=100000)
+
+preprocessing<-function(rawdata){
+  
+  ## Changing Datatypes
+  
+  rawdata[,7]<-as.integer(rawdata[,7])
+  rawdata[,8]<-as.integer(rawdata[,8])
+  rawdata[,3]<-as.numeric(rawdata[,3])
+  rawdata[,4]<-as.character(rawdata[,4])
+  rawdata[,6]<-as.character(rawdata[,6])
+  rawdata[,1]<-as.character(rawdata[,1])
+  rawdata[,2]<-as.character(rawdata[,2])
+  rawdata[,5]<-as.character(rawdata[,5])
+  
+  ## Identifying columns with Missing Values
+  colSums(is.na(rawdata))
+  
+  ## Selecting only rows with complete values
+  rawdata<-rawdata[complete.cases(rawdata),]
+  
+  ## Parsing Date Column in Raw Data
+  
+  for(i in 1:nrow(rawdata)){
+    position1<-regexpr('\\[',rawdata[i,1])
+    position2<-regexpr('\\]',rawdata[i,1])
+    rawdata[i,1]<-substr(rawdata[i,1],position1[1]+1,position2[1]-1)
+    
+    
+  }
+  ##  rawdata[,1]<-as.Date(rawdata[,1])
+  return(rawdata)
+}
+
+
+## Feature Extraction ...........................
+
+feature_extraction<-function(processed_rawdata){
+  
+  
+  names(processed_rawdata)<-c("Date","Tweet_Text","Tweet_id","User_id","User_Name","User_Screen_Name","Retweets","Favorites","Class")
+  
+  ## Removing white spaces in Tweet text
+  
+  
+  processed_rawdata$Tweet_Text<-str_trim(processed_rawdata$Tweet_Text)
+  
+  ## Finding Length of Tweet Text
+  processed_rawdata["Length of Tweet"]<-nchar(processed_rawdata$Tweet_Text)
+  
+  ## Finding Number of @ Mentions
+  processed_rawdata["No_of_@ Mentions"]<-str_count(processed_rawdata$Tweet_Text,pattern="@")
+  
+  ## Finding Number of Hashtags
+  processed_rawdata["No_of_Hashtags"]<-str_count(processed_rawdata$Tweet_Text,pattern="#")
+  
+  ## Finding Length of Screen Name
+  processed_rawdata["Length of Screen Name"]<-nchar(processed_rawdata$User_Screen_Name)
+  
+  ## ********** For Finding No of Emoticons **********
+  
+  emoticons <- c(":\\)",":-\\(","\\):",":S","o_O","=D")
+  
+  processed_rawdata["No_of_Emoticons"]<-str_count(processed_rawdata$Tweet_Text,paste0(emoticons, collapse="|"))
+  
+  ## *********** For Finding No of URL ***********
+  processed_rawdata["No_of_URL"]<-str_count(processed_rawdata$Tweet_Text,pattern="http://|https://")
+  
+  ## To find frequency of Spam Words
+  
+  tweettext<-as.vector(processed_rawdata$Tweet_Text)
+  
+  counter<-0
+  for(j in 1:length(tweettext)){
+    
+    for(i in 1:nrow(frame1)){
+      count_spam_words<-str_count(tweettext[j],coll(pattern=frame1[i,1],ignore_case=TRUE))
+      counter<-counter+count_spam_words
+      
+    }
+    processed_rawdata["No_of_Spam_Words"]<-counter
+    counter<-0
+  }
+  
+  ##  To find frequency of Swear Words
+  
+  counter2<-0
+  for(j in 1:length(tweettext)){
+    
+    for(i in 1:nrow(frame2)){
+      count_swear_words<-str_count(tweettext[j],coll(pattern=frame2[i,1],ignore_case=TRUE))
+      counter2<-counter2+count_swear_words
+      
+    }
+    processed_rawdata["No_of_Swear_Words"]<-counter2
+    counter2<-0
+  }
+  return(processed_rawdata)
+}
+
+## Feature Selection....
+
+feature_selection<-function(feature_extracted_data){
+  
+  for(i in 1:nrow(feature_extracted_data)){
+    feature_extracted_data[i,"New_Feature"]<-sum(
+      feature_extracted_data$`No_of_@ Mentions`[i],feature_extracted_data$No_of_Hashtags[i],
+      feature_extracted_data$No_of_Emoticons[i],feature_extracted_data$No_of_URL[i],
+      feature_extracted_data$No_of_Spam_Words[i],feature_extracted_data$No_of_Swear_Words[i])
+    
+    
+  }
+  
+  visualizing_data<-data.frame("Retweets"=feature_extracted_data$Retweets,
+                               "Favorites"=feature_extracted_data$Favorites,"New_Feature"=feature_extracted_data$New_Feature,"Class"=feature_extracted_data$Class)
+  return(visualizing_data)
+}
+
+
 fit<-function(training_data){
   
   optimized_weight<-c()
@@ -122,7 +244,6 @@ predict<-function(predict_us,svm_fit_data){
   return(return_list)
 }
 
-
 visualize<-function(training_data){
   x<-training_data[,1]
   y<-training_data[,2]
@@ -135,9 +256,44 @@ visualize<-function(training_data){
   }
   
   points3d(x,y,z,col=as.integer(training_data[,"Class"]))
-}  
+}
+
+process<-function(){
+  options(java.parameters = "-Xmx4096m")
+  
+  rawdata<-read.csv("RawDataSet.csv",header=FALSE)
+  ##names(rawdata)<-c("Date","Tweet_Text","Tweet_id","User_id","User_Name","User_Screen_Name","Retweets","Favorites")
+  
+  ## Loading swear words file
+  frame2<-read.csv("swear_words.csv",header=FALSE)
+  frame2<-as.matrix(frame2)
+  
+  ## Loading spam words file 
+  
+  frame1<-read.csv("Spam_words.csv",header=FALSE)
+  frame1<-as.matrix(frame1)
+  
+  processed_rawdata<-preprocessing(rawdata)
+  feature_extracted_data<-feature_extraction(processed_rawdata)
+  training_data<-feature_selection(feature_extracted_data)
+  
+  
+  
+  ## Store in some location ......
+  ## Writing Cleaned Dataset File
+  
+  write.table(processed_rawdata,"Cleaned.csv", sep=",",row.names=FALSE)
+  
+  ## Writing Feature Extracted Dataset File
+  write.table(feature_extracted_data,"Feature_Extraction_Dataset.csv", sep=",",row.names=FALSE)
+  
+  ## Writing Training Set file
+  write.table(training_data,"Training_Dataset.csv", sep=",",row.names=FALSE)
+}
 
 print("Main Program")
+
+#process()
 
 # Training the SVM
 sample<-read.csv("Training_Dataset.csv",header=TRUE)
@@ -147,7 +303,7 @@ svm_fit_data<-fit(sample)
 
 #Loading the test data
 #test_data_svm<-list(c(1,10,1),c(1,3,4),c(3,4,7),c(3,5,2),c(5,5,1),c(5,6,8),c(6,-5,1),c(6,-3,1),c(5,8,7), c(4,8,5))
-test_sample<-read.csv("Training_Dataset_test_data_700.csv",header=TRUE)
+test_sample<-read.csv("Test_Dataset.csv",header=TRUE)
 test_sample<-as.matrix(test_sample)
 test_data<- list()
 for (i in 1:nrow(test_sample)) {
