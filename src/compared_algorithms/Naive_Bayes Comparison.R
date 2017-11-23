5b4789c35988b336eb9210e9901a952941523936
simarpreet <simarpreet65@gmail.com>
2017-11-23 16:43:33 -0500

    Changed Naive Bayes Comparison code

--- src/compared_algorithms/Naive_Bayes Comparison.R
+++ src/compared_algorithms/Naive_Bayes Comparison.R
@@ -1,12 +1,9 @@
 ## Naive Bayes Comparison
 
-install.packages("naivebayes")
-library(naivebayes)
 
-setwd("C:/Users/Simarpreet Singh/Desktop/Data Mining Project - Github Clone/fake_news_detection_twitter/src")
 
 ## Loading Test Data Set
-test_set<-read.csv("Training_Dataset_1.csv",header=TRUE)
+test_set<-read.csv("Test_Dataset.csv",header=TRUE)
 
 ## Creating model for Naive Bayes
 naivebayes<-naive_bayes(test_set[,-4],as.factor(test_set[,4]))
@@ -15,4 +12,6 @@ naivebayes<-naive_bayes(test_set[,-4],as.factor(test_set[,4]))
 pred<-predict(naivebayes)
 
 ## creating Confusion Matrix
-confusionMatrix(test_set[,4],pred)
+cm<-confusionMatrix(test_set[,4],pred)
+
+return(cm$value)
