ca4b93d8c3bdabc7953c6830192742d8a2e38bee
prasanth <santhtce@gmail.com>
2017-11-23 15:17:30 -0500

    Compared the accuracy of all algorithms using bar plot

--- src/DM.R
+++ src/DM.R
@@ -1,2 +1,18 @@
+compute_accuracy<-function(cm) {
+  accuracy<-(cm[1] + cm[4])*100/(cm[1]+cm[2]+cm[3]+cm[4])
+}
+
 svm_confusion_matrix<-source('svm_3d.R')
+svm_accuracy<- compute_accuracy(svm_confusion_matrix$value)
+
 knn_confusion_matrix<-source('compared_algorithms/KNN.R')
+knn_accuracy<- compute_accuracy(knn_confusion_matrix$value)
+
+#Classifier names and their accuracy for the test data
+classifier_names = c("SVM", "KNN")
+classifier_accuracy = c(svm_accuracy, knn_accuracy)
+
+#Comparing the accuracy of the algorithms
+barplot(classifier_accuracy, main="Comparison of accuracy of different classifiers", 
+        ylim=c(0,100), xlab="Classifiers", ylab="Accuracy", names.arg= classifier_names ,col='green')
+axis(2,at=seq(0,100,10))
--- src/utitlities/bar.R
+++ src/utitlities/bar.R
@@ -11,3 +11,7 @@ barplot(mat,main="Accuracy of classifier using training data",
         xlab="Classes", col=c("green","red"), legend = rownames(mat))
 barplot(mat,main="Accuracy of classifier using training data",
         xlab="Classes", col=c("green","red"), legend = rownames(mat))
+
+B <- c(3, 2, 25, 37, 22, 34, 19)
+barplot(B, main="MY NEW BARPLOT", ylim = c(0,100), col = rainbow(20), xlab="LETTERS", ylab="MY Y VALUES", names.arg=c("A","B","C","D","E","F","G"))
+axis(2,at=seq(0,100,10))
